0e6fb13268ff7a952439efd606ae9c278568ae2a
tkddls8848 <tkddls8848@gmail.com>
2019-02-05 16:20:07 +0900

    정적 페이지 호출, 동적 페이지 호출

--- app.js
+++ app.js
@@ -5,8 +5,40 @@ var port = '3000';
 var hostname = '127.0.0.1';
 
 app.use(express.static('public'));
+
+app.get('/dynamic', function(req, res){
+	
+	var date = Date();
+	var list = '';
+	for(var i=0 ; i < 5 ; i++){
+		list += '<li>coding</li>'
+		}
+	;
+	var page =
+	`
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+				<title>Insert title here</title>
+		</head>
+		<body>
+			hello, dynamic!!!<br>
+			dynamic은 내용 수정시 서버재시작을 요함.<br><br>
+			
+			<ul>
+				${list}
+			</ul>
+			
+			${date}
+		</body>
+	</html>
+	`;
+	res.send(page);
+});
+
 app.get('/router',function(req, res){
-	res.send('hello router, <img src="/1.jpg">')	
+	res.send('hello router, <img src="/1.jpg">');
 });
 
 app.get('/',function(req, res){
