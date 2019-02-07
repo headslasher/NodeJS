f25cf2751e590572cca33f724893eebbdebefcea
tkddls8848 <tkddls8848@gmail.com>
2019-02-07 12:35:08 +0900

    jade 템플릿 문법, 자바기능구현

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var hostname = '127.0.0.1';
 app.use(express.static('public'));
 
 app.set('view engine','jade');
-app.set('views','./views')
+app.set('views','./views');
+
+app.locals.pretty = true;/*템플릿 들여쓰기*/
+
 app.get('/template', function(req, res){
-	res.render('temp');
+	res.render('temp', {date:Date(), string:'String'});
 });
 
 app.get('/dynamic', function(req, res){
@@ -17,9 +20,8 @@ app.get('/dynamic', function(req, res){
 	var date = Date();
 	var list = '';
 	for(var i=0 ; i < 5 ; i++){
-		list += '<li>coding</li>'
+		list += '<li>coding</li>';
 		}
-	;
 	var page =
 	`
 	<!DOCTYPE html>
@@ -47,14 +49,10 @@ app.get('/router',function(req, res){
 	res.send('hello router, <img src="/1.jpg">');
 });
 
-app.get('/',function(req, res){
-	res.send('going through / url');
-});
-
 app.get('/login', function(req, res){
 	res.send('going through /login url');
 });
 
 app.listen(3000, function() {
-	console.log('server port 3000 hostname ${hostname}');
+	console.log('server port 3000 connected');
 });
