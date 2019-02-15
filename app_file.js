d86f5c07196f85510576def1f6f56e4bccf3f763
tkddls8848 <tkddls8848@gmail.com>
2019-02-15 21:42:46 +0900

    readdir을 이용한 기존 파일 불러오기, pug 템플릿 엔진으로 글목록 만들기

--- app_file.js
+++ app_file.js
@@ -8,10 +8,21 @@ app.set('view engine', 'pug');
 app.locals.pretty=true;
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/topic/new', (req, res)=>{
+app.get('/topic/main', (req, res)=>{
 	
 	res.render('Hi');
 	
+});
+
+app.get('/topic', (req, res)=>{
+	
+	fs.readdir('data', (err, files) => {
+		if(err){
+			res.status(500).send('internal server error!!')
+		}
+		res.render('view', {topics:files});
+	})
+
 });
 
 app.post('/topic', (req, res)=>{
