e85ed1b93769cd12f52e8c96fa29ea903678aedb
tkddls8848 <tkddls8848@gmail.com>
2019-02-18 21:19:13 +0900

    파일 생성, 삭제, 조회 구현

--- app_file.js
+++ app_file.js
@@ -8,34 +8,53 @@ app.set('view engine', 'pug');
 app.locals.pretty=true;
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/topic/main', (req, res)=>{
+app.get('/topic/write', (req, res)=>{
 	
-	res.render('Hi');
+	res.render('write');
 	
 });
 
-app.get('/topic', (req, res)=>{
+app.get(['/topic', '/topic/:title'], (req, res)=>{
 	
 	fs.readdir('data', (err, files) => {
+		let title = req.params.title;
+		console.log(title);
+		
 		if(err){
 			res.status(500).send('internal server error!!')
 		}
-		res.render('view', {topics:files});
+		
+		if(title){
+			fs.readFile('data/'+title, 'UTF-8',  (err, data) => {
+				if(err){
+					res.status(500).send('internal server error!!')
+				}
+				res.render('main', {topics:files, title:title, content:data});
+			})			
+		} else {
+			res.render('main', {topics:files, title:'Welcome', content:'Homepage'});
+		}
 	})
-
 });
 
-app.post('/topic', (req, res)=>{
+app.post('/topic/write', (req, res)=>{
 	
-	let text = req.body.text;
-	let textarea = req.body.textarea;
+	let title = req.body.title;
+	let content = req.body.content;
 	
-	fs.writeFile('data/'+text, textarea, (err) => {
+	fs.writeFile('data/'+title, content, (err) => {
 		if(err){
 			res.status(500).send('internal server error!!');
 		}
-		res.send(' text = '+text+' textarea = '+textarea);
+		res.redirect('/topic');
 	});
+});
+
+app.post('/topic/delete', (req, res)=>{
+	
+	fs.unlink(path, function() {
+		
+	})
 });
 
 app.listen(4000, ()=>{
