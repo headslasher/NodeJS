41d2704c07eb7a4c6c5723fc989eb76557461fe2
tkddls8848 <tkddls8848@gmail.com>
2019-03-06 20:09:42 +0900

    cookie 리스트 데이터 받아서 화면에 표현

--- app_cookielist.js
+++ app_cookielist.js
@@ -10,14 +10,14 @@ let products = {
 app.get('/product',(req,res)=>{
 
 	let content = '';
-	
+
 	for(let name in products){
-		content += 
+		content +=
 		`
 		<li><a href='/cart/${name}'>${products[name].title}</a></li>
 		`
 	}
-	
+
 	res.send(
 			`<ul>${content}</ul>`);
 });
@@ -25,20 +25,40 @@ app.get('/product',(req,res)=>{
 app.get('/cart/:id', (req,res)=>{
 
 	let id = req.params.id;
-	
+
 	if(req.cookies.cart){
 		var cart = req.cookies.cart;
 	} else {
 		var cart = {};
 	}
-	
+
 	if(!cart[id]){
 		cart[id] = 0;
-	} 
+	}
 	cart[id] = parseInt(cart[id])+1;
 	res.cookie('cart',cart);
-	res.send(`<a href='/product'>`+'{'+req.params.id+':'+cart[id]+'}'+`</a>`);
-	
+	res.redirect('/cart');
+});
+
+app.get('/cart',(req, res)=>{
+
+	let cart = req.cookies.cart;
+
+	let str = '';
+
+	for(let num in products){
+		if(!cart[num]){
+			cart[num] = 0;
+		}
+		str += `<li>${products[num].title}:${cart[num]}</li>`
+	}
+
+	res.send(`
+		<ul>
+			${str}
+		</ul>
+		<a href='/product'>`+'ListPage'+`</a>
+		`);
 });
 
 app.listen(3000,()=>{
