dc60a98229dc1a34fbca714356c72fdaa083d21d
tkddls8848 <tkddls8848@gmail.com>
2019-03-12 15:16:02 +0900

    orientDB를 이용한 세션 관리, 비밀번호 암호화(md5, salt)

--- app_session.js
+++ app_session.js
@@ -1,12 +1,17 @@
 const app = require('express')();
 const session = require('express-session');
+const OrientoStore = require('connect-oriento')(session);
 const bodyparser = require('body-parser');
+const md5 = require('md5');
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(session({
-	secret:'13r13r#R!#',
+	secret:'13r133tw3tr#R!#',
 	resave: false,
-	saveUninitialized : true
+	saveUninitialized : true,
+	store: new OrientoStore({
+	    server: 'host=localhost&port=2424&username=root&password=password&db=demodb'
+	})
 }));
 
 app.get('/count',(req, res)=>{
@@ -36,28 +41,42 @@ app.get('/auth/login',(req, res)=>{
 });
 
 app.post('/auth/login',(req, res)=>{
+	const userDB=[{
+									salt : '!13t!#TG@G@$',
+									id:'admin',
+									pw:'cffa238e1cd60d78020cd97e44a39ca6',
+									name:'adminname'
+								},
+								{
+									salt : '!198wg3oihw3G@G@$',
+									id:'admin1',
+									pw:'101f4b240a8ce811dfe58b4d7aacaf9a',
+									name:'adminname1'
+								}];
 
-	const admin={
-		id:'admin',
-		pw:'admin',
-		name:'adminname'
-	};
 	let uid = req.body.id;
 	let upw = req.body.pw;
-	if(uid==admin.id){
-		req.session.uid = admin.id;
-		res.redirect('/welcome');
-	} else if (req.body.id == null){
-		res.redirect('/auth/login');
+
+	for(let i =0;i<userDB.length;i++){
+		if(uid == userDB[i].id && md5(upw+userDB[i].salt) == userDB[i].pw){
+			req.session.uid = userDB[i].id;
+			req.session.upw = userDB[i].pw;
+			return req.session.save(() => {
+						res.redirect('/welcome');
+			})
+		}
+	}
+	if (req.body.id == null){
+	res.redirect('/auth/login');
 	} else {
-		req.session.uid = req.body.id;
-		let output=`
-			<div>
-				user page
-				<a href='/welcome'>welcome</a>
-			</div>
-		`;
-		res.send(output);
+	req.session.uid = req.body.id;
+	let output=`
+		<div>
+			user page
+			<a href='/welcome'>welcome</a>
+		</div>
+	`;
+	res.send(output);
 	}
 });
 
@@ -66,7 +85,7 @@ app.get('/welcome',(req, res)=>{
 	if(req.session.uid=='admin'){
 		let output=`
 			<div>
-				admin Welcome!
+				${req.session.uid} Welcome!
 				<a href='/auth/logout'>logout</a>
 			</div>
 		`;
@@ -79,23 +98,16 @@ app.get('/welcome',(req, res)=>{
 			</div>
 		`;
 		res.send(output);
-	} else {
-		let output=`
-			<div>
-				${req.session.uid} Welcome!
-				<a href='/auth/logout'>logout</a>
-			</div>
-		`;
-		res.send(output);
 	}
 });
 
-
 app.get('/auth/logout',(req, res)=>{
 	delete req.session.uid;
-	res.redirect('/welcome');
+	req.session.save(function(){
+		res.redirect('/auth/login');
+	});
 });
 
-app.listen('3001',()=>{
-	console.log('3001 connected');
+app.listen('3000',()=>{
+	console.log('3000 connected');
 });
