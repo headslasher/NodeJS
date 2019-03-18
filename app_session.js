37c3f6e4f08f17a245cd59deecf5519edd72659d
tkddls8848 <tkddls8848@gmail.com>
2019-03-18 15:11:28 +0900

    pbkdf2-password모듈 추가

--- app_session.js
+++ app_session.js
@@ -2,9 +2,29 @@ const app = require('express')();
 const session = require('express-session');
 const OrientoStore = require('connect-oriento')(session);
 const bodyparser = require('body-parser');
+const hasher = require("pbkdf2-password")();
+const assert = require("assert");
+const opts = {
+  password: "helloworld"
+};
 const md5 = require('md5');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+hasher(opts, (err, pass, salt, hash) => {
+ opts.salt = salt;
+ hasher(opts, (err, pass, salt, hash2) => {
+	 assert.deepEqual(hash2, hash);
+
+	 // password mismatch
+	 opts.password = "aaa";
+	 hasher(opts, (err, pass, salt, hash2) => {
+		 assert.notDeepEqual(hash2, hash);
+		 console.log("OK");
+	 });
+ });
+});
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(session({
@@ -17,6 +37,37 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new LocalStrategy(
+	(username, password, done)=>{
+			var userDB={//111111
+											salt : 'pWZXLF3Mul3VJ/HsRVfVRHo+HRZxVB5qNZAfCnMkWLTMHZFl3T6ImShrQDjJDUkszXVjB0QKg2FVro1YI1WO0w==',
+											id:'admin',
+											pw:'1jSJOKDAgwreV+gyV7bvK7n8hLEkwsEVcWlDemEhz1WyAGZmBx7Jr+I/I43yj6Zn8ezJC9N+8qeMLPWYgliUXQS53yx3U/MQgFhWTT2fqMB+CRAsRcAVjTjLetWdpJXfTWPkq8BYWMYxCXsJkbiHrLd1lJyUYodR3dtp7CGQEYM=',
+											name:'adminname'
+									};
+
+			let uid = username;
+			let upw = password;
+
+			for(let i =0;i<userDB.length;i++){
+				let user = userDB[i];
+				if(uid == user.id && md5(upw+user.salt) == user.pw){
+					console.log(uid+'and'+upw);
+					done(null, user);
+				} else {
+					console.log(uid+'and'+upw);
+					done(null, false);
+				}
+			}
+			done(null, false);
+			}
+));
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
+});
 
 app.get('/count',(req, res)=>{
 	res.redirect('/tmp');
@@ -43,37 +94,6 @@ app.get('/auth/login',(req, res)=>{
 	`;
 	res.send(output);
 });
-
-passport.use(new LocalStrategy(
-	(username, password, done)=>{
-
-			const userDB=[{
-											salt : '!13t!#TG@G@$',
-											id:'admin',
-											pw:'cffa238e1cd60d78020cd97e44a39ca6',
-											name:'adminname'
-										},
-										{
-											salt : '!198wg3oihw3G@G@$',
-											id:'admin1',
-											pw:'101f4b240a8ce811dfe58b4d7aacaf9a',
-											name:'adminname1'
-										}];
-
-			let uid = username;
-			let upw = password;
-
-			for(let i =0;i<userDB.length;i++){
-				let user = userDB[i];
-				if(uid == user.id && md5(upw+user.salt) == user.pw){
-					done(null, user);
-				} else {
-					done(null, false);
-				}
-			}
-			done(null, false);
-			}
-));
 
 app.post('/auth/login',
   passport.authenticate('local', { successRedirect: '/welcome',
